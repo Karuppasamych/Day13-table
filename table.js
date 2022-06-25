88a468670381800015fe9c32dcf767524eaebd7a
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-25 23:16:56 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,7 @@
+let j = prompt("Enter the number of table")
+let table = 1, mul=0;
+while( table <= 10 ){
+    mul = j * table
+    console.log("printing a table", j,"*" ,table,"=", mul);
+    table++;
+}
